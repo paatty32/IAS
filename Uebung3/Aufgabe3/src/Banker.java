99e894c125f255f613643f417f0c0e02a1ce31b6
paatty32 <patrick.boadu.boafo@hotmail.de>
2020-11-20 19:39:07 +0100

    Erste klassen und Methoden geschrieben

---
+++ Uebung3/Aufgabe3/src/Banker.java
@@ -0,0 +1,20 @@
+public class Banker extends Person {
+
+    public Banker(String name, int einkommen ){
+        super(name, einkommen);
+    }
+
+    /**
+     * Berechnet die Steuer eines Bankers.
+     * Bank muss während der gegenwärtigen Krise von der Regierung 1000 pfund steuern zusätlich zahlen.
+     * Wenn der zu zahlende Betrag größer als das einkommen ist, wird das einkommen zurückgegben.
+     * @return zu zahlende Steuer.
+     */
+    @Override
+    public int steuer() {
+        int steuer = super.steuer() + 1000;
+
+
+        //if()
+    }
+}
---
+++ Uebung3/Aufgabe3/src/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+
+    private int einkommen; //tatsächliche Jahreseinkommen
+    private final double STEUER = 0.25; //Konstante, die bei bedarf geändert werden kann.
+    String name;
+
+    /**
+     * Konstruktor
+     * @param name Name der Person
+     * @param einkommen Einkommen der Peson
+     */
+    public Person(String name, int einkommen){
+        this.einkommen = einkommen;
+        this.name = name;
+    }
+    /**
+     * Das zu versteurende Einkommen entspricht dem tatsächlichen Einkommen
+     * einer Person.
+     * @return einkommen.
+     */
+    public int zuVersteuerndesEinkommen(){
+        return this.einkommen;
+    }
+
+    /**
+     * Berechnet die Steuer fuer eine Person, die er zu Zahlen hat.
+     * @return abgerunderter Steuer Betrag.
+     */
+    public int steuer(){
+        double steuer = this.einkommen * this.STEUER;
+
+        return (int) steuer;
+
+    }
+
+
+
+}
